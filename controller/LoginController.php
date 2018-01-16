ebcfd1c92c005b9fbbc2e46525036641f2f568b7
Ruan ZeTao <ruanzetao@gmail.com>
2018-01-17 00:26:55 +0700

    bugController

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -1,16 +1,14 @@
 <?php
-require_once('Controller.php');
-require_once('../model/LoginModel.php');
-require_once('../model/connect.php');
-
+include("Controller.php");
 class LoginController extends Controller{
 	public function getLogin(){
 		$_Username=isset($_POST['username'])?$_POST['username']:'';
 		$_Password=isset($_POST['password'])?$_POST['password']:'';
+		echo "01234";
 
 		if($_Password!=''&&$_Username!=''){
 			$_LoginModel=new LoginModel();
-			$result=$_LoginModel->login($username,$password);
+			$result=$_LoginModel->login($_Username,$_Password);
 			if($result->num_rows>0){
 				echo "123";
 				//header('Location: view/trangchu.php');
