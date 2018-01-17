2f027cbb4c4ad671cfa2f58c359fdb6a1cd5d012
Ruan ZeTao <ruanzetao@gmail.com>
2018-01-17 23:03:21 +0700

    done fix bug1

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -2,11 +2,16 @@
 include('Controller.php');
 //include("model/connect.php");
 include('model/LoginModel.php');
+include('view/login.php');
 
 class LoginController extends Controller{
 	public function getLogin(){
 		$_Username=isset($_POST['username'])?$_POST['username']:'';
 		$_Password=isset($_POST['password'])?$_POST['password']:'';
+
+		echo $_Username;
+		echo "<br>";
+		echo $_Password;
 
 		//Bi loi cho nay
 			$_LoginModel=new LoginModel();
--- view/login.php
+++ view/login.php
@@ -62,14 +62,14 @@
 							<a href="#" class="facebook-sign-in"><i class="fa fa-facebook"></i> Sign In with Facebook</a>
 							<a href="#" class="twitter-sign-in"><i class="fa fa-twitter"></i> Sign In with Twitter</a>
 						</div>
-						<form class="register-form outer-top-xs" role="form" action="" method="POST">
+						<form class="register-form outer-top-xs" role="form" action="" method="post">
 							<div class="form-group">
 								<label class="info-title" for="exampleInputEmail1">Email Address <span>*</span></label>
-								<input type="text/css" class="form-control unicase-form-control text-input" id="email" name="email" >
+								<input type="text/css" class="form-control unicase-form-control text-input" id="username" name="username" >
 							</div>
 							<div class="form-group">
 								<label class="info-title" for="exampleInputPassword1">Password <span>*</span></label>
-								<input type="password" class="form-control unicase-form-control text-input" id="password" name="password" >
+								<input type="text/css" class="form-control unicase-form-control text-input" id="password" name="password" >
 							</div>
 							<div class="radio outer-xs">
 								<label>
