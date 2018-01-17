58b684253d040647b02c2dd65f172785001dd2ef
Ruan ZeTao <ruanzetao@gmail.com>
2018-01-17 16:53:45 +0700

    remove comment

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -6,22 +6,22 @@ class LoginController extends Controller{
 	public function getLogin(){
 		$_Username=isset($_POST['username'])?$_POST['username']:'';
 		$_Password=isset($_POST['password'])?$_POST['password']:'';
-		echo "01234";
+		//echo "01234";
 
 		if($_Password!=''&&$_Username!=''){
 			$_LoginModel=new LoginModel();
 			$result=$_LoginModel->Login($_Username,$_Password);
 			if($result->num_rows>0){
-				echo "123";
-				//header('Location: view/trangchu.php');
+				//echo "123";
+				header('Location: view/trangchu.php');
 			}
 			else{
-				echo "456";
-				//header('Location: view/login.php');
+				//echo "456";
+				header('Location: view/login.php');
 			}
 			$db->close();
 		}
-		//return $this->loadView('login');
+		return $this->loadView('login');
 	}
 }
 
