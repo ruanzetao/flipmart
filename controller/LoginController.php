aa84c818af39418a5c8500512f4b87c7a4d7823b
Ruan ZeTao <ruanzetao@gmail.com>
2018-01-18 23:10:44 +0700

    commit

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -2,30 +2,27 @@
 include('Controller.php');
 //include("model/connect.php");
 include('model/LoginModel.php');
-include('view/login.php');
+include('view/sign-in.php');
 
 class LoginController extends Controller{
 	public function getLogin(){
 		$_Username=isset($_POST['username'])?$_POST['username']:'';
 		$_Password=isset($_POST['password'])?$_POST['password']:'';
 
-		echo $_Username;
-		echo "<br>";
-		echo $_Password;
+		// echo $_Username;
+		// echo "<br>";
+		// echo $_Password;
 
 		//Bi loi cho nay
 			$_LoginModel=new LoginModel();
 			$result=$_LoginModel->Login($_Username,$_Password);
 			//echo "8910";
 			if(count($result)>0){
-				echo "123";
-				return $this->loadView('home');
+				return $this->loadView('view/home.php');
 			}
 			else{
-				echo "456";
-				return $this->loadView('login');
+				return $this->loadView('view/sign-in.php');
 			}
-			//$db->close();
 		
 
 		//return $this->loadView('login');
